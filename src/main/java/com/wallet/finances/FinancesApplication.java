5d273c65800284d1a14124e7052e14b044d386ea
Luiz Filipe <luizfilipemr17@gmail.com>
2023-06-14 21:46:33 -0300

    quickstart

---
+++ src/main/java/com/wallet/finances/FinancesApplication.java
@@ -0,0 +1,13 @@
+package com.wallet.finances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancesApplication.class, args);
+	}
+
+}
