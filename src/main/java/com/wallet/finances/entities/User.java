d8ff00f34e498653a9aebad5daa7e7a02bae7d84
Luiz Filipe <luizfilipemr17@gmail.com>
2023-06-19 11:26:40 -0300

    Basic Impl UserDAO

--- src/main/java/com/wallet/finances/entities/User.java
+++ src/main/java/com/wallet/finances/entities/User.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Id;
 public class User implements Serializable{
 
     private static final long serialVersionUID = 1L;
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
