0ccf982ff88b1e5262d53b128683b2de9ac3ba0f
nivaldodev <nivaldoalmeida.dev@gmail.com>
2023-06-27 21:06:13 -0400

    category-rest-controller

--- src/main/java/com/wallet/finances/entities/Category.java
+++ src/main/java/com/wallet/finances/entities/Category.java
@@ -24,6 +24,14 @@ public class Category implements Serializable {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
---
+++ src/main/java/com/wallet/finances/rest/CategoryRestController.java
@@ -0,0 +1,43 @@
+package com.wallet.finances.rest;
+
+import com.wallet.finances.dao.CategoryRepository;
+import com.wallet.finances.entities.Category;
+import com.wallet.finances.services.CategoryService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CategoryRestController {
+    private CategoryService categoryService;
+    public CategoryRestController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+    @GetMapping("/categories")
+    public List<Category> findAll(){
+        return categoryService.findAll();
+    }
+    @PostMapping("/categories")
+    public Category addCategory(@RequestBody Category category){
+        category.setId(0L);
+        return categoryService.save(category);
+    }
+    @DeleteMapping("/categories/{id}")
+    public String deleteCategory(@PathVariable Long id){
+        if(categoryService.findById(id) == null){
+            throw new RuntimeException("Category not found");
+        }
+        categoryService.deleteById(id);
+        return "Category with id: "+ id +" deleted successfully";
+    }
+    @GetMapping("/categories/{id}")
+    public Category findById(@PathVariable Long id){
+        return categoryService.findById(id);
+    }
+    @PutMapping("/categories")
+    public Category updateCategory(@RequestBody Category category){
+        return categoryService.save(category);
+    }
+}
