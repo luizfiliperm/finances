bd16aebd7263039b221cfcb55f426569c308aee7
luizfiliperm <luizfilipemr17@gmail.com>
2023-07-06 09:32:11 -0300

    Refactor UserRepository

--- src/main/java/com/wallet/finances/dao/UserRepository.java
+++ src/main/java/com/wallet/finances/dao/UserRepository.java
@@ -1,16 +1,13 @@
 package com.wallet.finances.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.wallet.finances.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
     
-    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.email = :email")
-    boolean existsByEmail(String email);
+    Boolean existsByEmail(String email);
 
-    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.username = :username")
-    boolean existsByUsername(String username);
+    Boolean existsByUsername(String username);
     
 }
