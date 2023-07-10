acd1ff384e5c828e995e0c46881d06973ac1b7df
luizfiliperm <luizfilipemr17@gmail.com>
2023-07-10 20:44:46 -0300

    Adding JWT Dependence, COnfiguring Token Service, and adding the Security Token on application.properties

---
+++ src/main/java/com/wallet/finances/infra/security/TokenService.java
@@ -0,0 +1,43 @@
+package com.wallet.finances.infra.security;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.wallet.finances.entities.user.User;
+
+
+@Service
+public class TokenService {
+    
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String genToken(User user){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            String token = JWT.create()
+                           .withIssuer("authAPI")
+                           .withSubject(user.getUsername())
+                           .withExpiresAt(genExpirationDate())
+                           .sign(algorithm);
+            return token;
+        }catch(JWTCreationException exception){
+            throw new RuntimeException("Error while generating token!");
+        }
+    }
+
+    
+    private Instant genExpirationDate(){
+        return LocalDateTime.now().plusHours(2L).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+
+}
