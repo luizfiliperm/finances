e0475ce92aace8d7c8d52fdbee9ea50ee25ea59b
luizfiliperm <luizfilipemr17@gmail.com>
2023-07-11 11:45:06 -0300

    Just the necessary in UserController

--- src/main/java/com/wallet/finances/rest/UserRestController.java
+++ src/main/java/com/wallet/finances/rest/UserRestController.java
@@ -2,12 +2,9 @@ package com.wallet.finances.rest;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,49 +14,21 @@ import com.wallet.finances.services.UserService;
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
-    
-    // private UserService userService;
 
-    // public UserRestController(UserService userService) {
-    //     this.userService = userService;
-    // }
+    @Autowired
+    private UserService userService;
 
-    // @GetMapping("/users")
-    // public List<User> findAll() {
 
-    //     return userService.findAll();
-    // }
+    @GetMapping("/users")
+    public List<User> findAll() {
 
-    // @GetMapping("/users/{userId}")
-    // public User getUser(@PathVariable Long userId) {
-    //     User user = userService.findById(userId);
+        return userService.findAll();
+    }
 
-    //     return user;
-    // }
+    @GetMapping("/users/{userId}")
+    public User getUser(@PathVariable Long userId) {
+        User user = userService.findById(userId);
 
-    // @PostMapping("/users")
-    // public User addUser(@RequestBody User user) {
-    //     user.setId(0L);
-
-    //     return userService.save(user);
-    // }
-
-    // @PutMapping("/users")
-    // public User updateUser(@RequestBody User user) {
-    //     return userService.save(user);
-    // }
-
-    // @DeleteMapping("/users/{userId}")
-    // public String deleteUser(@PathVariable Long userId) {
-
-    //     User user = userService.findById(userId);
-
-    //     if (user == null) {
-    //         throw new RuntimeException("User id not found - " + userId);
-    //     }
-
-    //     userService.deleteById(userId);
-        
-    //     return "Deleted user id - " + userId;
-    // }
+        return user;
+    }
 }
