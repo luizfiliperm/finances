b678e8528067629b2bc301b69283172f3d004d88
luizfiliperm <luizfilipemr17@gmail.com>
2023-07-16 10:50:39 -0300

    Refactor Utils

---
+++ src/main/java/com/wallet/finances/util/DateTimeUtil.java
@@ -0,0 +1,7 @@
+package com.wallet.finances.util;
+
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeUtil {
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("MM-dd-YYYY");
+}
