5ab3e3b0e60e988d68a3f2ecf165e307ebcbc2c9
luizfiliperm <luizfilipemr17@gmail.com>
2023-07-17 19:29:48 -0300

    Adding totalIncome and totalExpense calculation to wallet

--- src/main/java/com/wallet/finances/entities/wallet/Wallet.java
+++ src/main/java/com/wallet/finances/entities/wallet/Wallet.java
@@ -1,9 +1,11 @@
 package com.wallet.finances.entities.wallet;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.wallet.finances.entities.transactions.Transaction;
+import com.wallet.finances.entities.transactions.income.Income;
 import com.wallet.finances.entities.user.User;
 
 import jakarta.persistence.*;
@@ -22,6 +24,10 @@ public class Wallet {
 
     @OneToMany(mappedBy = "wallet", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Transaction> transactions;
+
+    private BigDecimal totalIncome;
+
+    private BigDecimal totalExpense;
 
     public Wallet() {
         transactions = new ArrayList<>();
@@ -53,6 +59,51 @@ public class Wallet {
 
     public void addTransaction(Transaction transaction){
         this.transactions.add(transaction);
+        calculateTotalIncome();
+        calculateTotalExpense();
+    }
+
+    public BigDecimal getTotalIncome() {
+        return totalIncome;
+    }
+
+    public void setTotalIncome(BigDecimal totalIncome) {
+        this.totalIncome = totalIncome;
+    }
+
+    public BigDecimal getTotalExpense() {
+        return totalExpense;
+    }
+
+    public void setTotalExpense(BigDecimal totalExpense) {
+        this.totalExpense = totalExpense;
+    }
+
+    public void calculateTotalIncome(){
+        BigDecimal totalIncome = BigDecimal.ZERO;
+
+        for(Transaction transaction : transactions){
+            if(transaction instanceof Income){
+                Income income = (Income) transaction;
+                totalIncome = totalIncome.add(income.getValue());
+            }
+        }
+
+        this.totalIncome = totalIncome;
+    }
+
+    public void calculateTotalExpense(){
+        BigDecimal totalExpense = BigDecimal.ZERO;
+
+        for(Transaction transaction : transactions){
+            if(transaction instanceof Income){
+                Income income = (Income) transaction;
+                totalExpense = totalExpense.add(income.getValue());
+            }
+        }
+
+        this.totalExpense = totalExpense;
     }
     
 }
+
