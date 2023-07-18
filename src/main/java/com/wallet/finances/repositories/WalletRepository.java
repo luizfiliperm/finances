bb3ee6bd962bf197bb6bedab7a7df9423fb626e1
luizfiliperm <luizfilipemr17@gmail.com>
2023-07-18 10:05:00 -0300

    Query to return a Wallet with a range date of transactions

--- src/main/java/com/wallet/finances/repositories/WalletRepository.java
+++ src/main/java/com/wallet/finances/repositories/WalletRepository.java
@@ -1,6 +1,10 @@
 package com.wallet.finances.repositories;
 
+import java.time.LocalDate;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.wallet.finances.entities.wallet.Wallet;
 
@@ -8,4 +12,6 @@ public interface WalletRepository extends JpaRepository<Wallet, Long> {
 
     Wallet findByUserUsername(String username);
     
+    @Query("SELECT DISTINCT w FROM Wallet w JOIN FETCH w.transactions t WHERE w.id = :walletId AND t.date BETWEEN :startDate AND :endDate")
+    Wallet findByIdAndDateRange(@Param("walletId") Long walletId, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 }
