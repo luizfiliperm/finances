4a7187147821b507ff34cba0f08a089459b824ef
luizfiliperm <luizfilipemr17@gmail.com>
2023-07-18 20:31:40 -0300

    Adding convertStringToLocalDate method

--- src/main/java/com/wallet/finances/util/DateTimeUtil.java
+++ src/main/java/com/wallet/finances/util/DateTimeUtil.java
@@ -1,7 +1,12 @@
 package com.wallet.finances.util;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtil {
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("MM-dd-yyyy");
+
+    public static LocalDate convertStringToLocalDate(String date) {
+        return LocalDate.parse(date, FORMATTER);
+    }
 }
